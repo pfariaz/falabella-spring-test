fc98ab997f20ecde239f54ae28d993e545b1a433
Pablo Farias <pfariaz011@gmail.com>
2020-04-03 15:06:28 -0300

    feat: first commit

---
+++ src/main/java/com/example/falabellaspringtest/FalabellaSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.falabellaspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalabellaSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalabellaSpringTestApplication.class, args);
+	}
+
+}
